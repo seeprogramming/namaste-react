8b78f22523f91354a3145f305ba0abea803488b8
Sagar Vilasrao Shelke <sagarshelke18@gmail.com>
2023-07-22 17:23:59 +0530

    Nested elements

--- App.js
+++ App.js
@@ -1,11 +1,41 @@
+{
+    /* <div id='parent'>
+    <div id="child">
+        <h1></h1>
+        <h2></h2>
+    </div>
+    <div id="child2">
+        <h1></h1>
+        <h2></h2>
+    </div>
+</div> */
+}
+
+const parent = React.createElement(
+    'div',
+    {
+        id: 'parent',
+    },
+    [
+        React.createElement('div', { id: 'child' }, [
+            React.createElement('h1', {}, 'I am h1'),
+            React.createElement('h2', {}, 'I am h2'),
+        ]),
+        React.createElement('div', { id: 'child2' }, [
+            React.createElement('h1', {}, 'I am h1'),
+            React.createElement('h2', {}, 'I am h2'),
+        ]),
+    ]
+);
+
 const heading = React.createElement(
     'h1',
     { id: 'heading' },
     'Hello React From Other file'
 );
 
-console.log(heading);
+console.log(parent);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(heading);
+root.render(parent);
